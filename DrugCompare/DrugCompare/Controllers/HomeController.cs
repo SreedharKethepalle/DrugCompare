5a3804feeb588535d61fb59d0865a0e97c41ec71
Muhil <MuhilarasanK@emids.com>
2019-12-11 16:32:22 +0530

    New Code
    Code added to git

---
+++ DrugCompare/DrugCompare/Controllers/HomeController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DrugCompare.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DrugCompare.Controllers
+{
+    public class HomeController : Controller
+    {
+        string conn = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Dashboard(int userId = 0)
+        {
+            Session["Plans"] = getPlansList(2019);
+
+            List<PlansList> _planNames = new List<PlansList>();
+            _planNames = (List<PlansList>)Session["Plans"];
+
+            var _dashboard = getDashBoardDetails(userId);
+            _dashboard.PlanLists = _planNames;
+
+            return View(_dashboard);
+        }
+
+        public ActionResult PlanList()
+        {
+            return RedirectToAction("Dashboard", new { userId = 100 });
+        }
+
+        private DashboardViewModel getDashBoardDetails(int userId)
+        {
+            DashboardViewModel _dashboard = new DashboardViewModel();
+            DataSet ds = new DataSet();
+
+
+            using (SqlConnection con = new SqlConnection(conn))
+            {
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+
+                    cmd.CommandText = "[dbo].[Sp_GetUserDetailsForDashboard]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UserId", userId);
+
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+
+                    con.Close();
+                }
+            }
+
+            _dashboard = dashboardList(ds);
+            
+
+            return _dashboard;
+        }
+
+        private DashboardViewModel dashboardList(DataSet ds)
+        {
+            DashboardViewModel _dashboard = new DashboardViewModel();
+
+            if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                List<SelectedPlanInfo> _UserSelectedPlans = new List<SelectedPlanInfo>();
+                _dashboard.SelectedPlanInfoVM = Common.ConvertToList<SelectedPlanInfo>(ds.Tables[0]); 
+            }
+
+            if (ds != null && ds.Tables.Count > 1 && ds.Tables[1].Rows.Count > 0)
+            {
+                List<SelectedPrescriptionViewModel> _Prescriptionn = new List<SelectedPrescriptionViewModel>();
+                _dashboard.SelectedPrescriptionVM = Common.ConvertToList<SelectedPrescriptionViewModel>(ds.Tables[1]); 
+            }
+
+            if (ds != null && ds.Tables.Count > 2 && ds.Tables[2].Rows.Count > 0)
+            {
+                List<SelectedProviderViewModel> _Provider = new List<SelectedProviderViewModel>();
+                _dashboard.SelectedProviderVM = Common.ConvertToList<SelectedProviderViewModel>(ds.Tables[2]);
+            }
+
+            if (ds != null && ds.Tables.Count > 3 && ds.Tables[3].Rows.Count > 0)
+            {
+                List<SelectedPharmacyViewModel> _Pharmacy = new List<SelectedPharmacyViewModel>();
+                _dashboard.SelectedPharmacyVM = Common.ConvertToList<SelectedPharmacyViewModel>(ds.Tables[3]);
+            }
+
+            return _dashboard;
+        }
+
+        private List<PlansList> getPlansList(int planYear)
+        {
+            List<PlansList> _plansList = new List<PlansList>();
+            DataSet ds = new DataSet();
+
+            using (SqlConnection con = new SqlConnection(conn))
+            {
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+
+                    cmd.CommandText = "[dbo].[Sp_GetPlansList]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@PlanYear", planYear);
+
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+
+                    con.Close();
+                }
+            }
+
+            _plansList = Common.ConvertToList<PlansList>(ds.Tables[0]);
+
+            return _plansList;
+        }
+    }
+}
+
+
+//DashboardViewModel _dashboard = new DashboardViewModel
+//{
+//    UserPlansVM = new UserPlanViewModel { PlanId = 102, PlanName = "halla gilla plan" },
+//    PrescriptionVM = new PrescriptionViewModel { DrugId = 101, DrugName = "Dolo", DoseId = 101, DoseageName = "Dolo - 650" },
+//    ProviderVM = new ProviderViewModel { ProviderId = 101, ProviderName = "Shree Krishna Clinic" },
+//    PharmacyVM = new PharmacyViewModel { PharmacyId = 101, PharmacyName = "Apollo Pharmacy" }
+//};
---
+++ DrugCompare/DrugCompare/Models/AccountViewModels.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DrugCompare.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Remember this browser?")]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class DashboardViewModel
+    {
+        public List<SelectedPlanInfo> SelectedPlanInfoVM { get; set; }
+        public List<SelectedPrescriptionViewModel> SelectedPrescriptionVM { get; set; }
+        public List<SelectedProviderViewModel> SelectedProviderVM { get; set; }
+        public List<SelectedPharmacyViewModel> SelectedPharmacyVM { get; set; }
+        public List<PlansList> PlanLists { get; set; }
+    }
+
+
+    public class SelectedPrescriptionViewModel
+    {
+        public int DrugId { get; set; }
+        public string DrugName { get; set; }
+        public int DoseId { get; set; }
+        public string DoseageName { get; set; }
+        public int DrugQuantity { get; set; }
+        public int DrugPrice { get; set; }
+
+        
+    }
+
+    public class SelectedProviderViewModel
+    {
+        public int ProviderId { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderAddress { get; set; }
+        public int ProviderPhone { get; set; }
+        public int ProviderExperience { get; set; }
+    }
+
+    public class SelectedPharmacyViewModel
+    {
+        public int PharmacyId { get; set; }
+        public string PharmacyName { get; set; }
+        public string PharmacyAddress { get; set; }
+        public int PharmacyPhone { get; set; }
+
+    }
+
+    public class SelectedPlanInfo
+    {
+        public int PlanId { get; set; }
+        public string PlanName { get; set; }
+        public int PlanTypeId { get; set; }
+        public string PlanTypeName { get; set; }
+        public int PlanYear { get; set; }
+
+    }
+
+    public class PlansList
+    { 
+        public int PlanId { get; set; }
+        public string PlanName { get; set; }
+
+    }
+
+
+
+
+
+}
