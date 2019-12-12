fb99553c04b246312cd546558df765997da607a6
Muhil <MuhilarasanK@MUHILARASANK02>
2019-12-12 15:23:13 +0530

    table column changes

--- Database_Scripts/13.1.Fk_PatientDrugMapping_DrugMaster1.sql
+++
@@ -1,6 +0,0 @@
-
-ALTER TABLE PatientDrugMapping
-ADD CONSTRAINT [Fk_PatientDrugMapping_DrugMaster1]
-FOREIGN KEY (DrugId)
-REFERENCES DrugMaster (DrugId)
-GO
--- Database_Scripts/5.1 Alter_PatientDrugMapping.sql
+++
@@ -1,9 +0,0 @@
-USE [DrugCompare]
-GO 
-
-IF NOT EXISTS(SELECT * FROM Sys.columns WHERE Name LIKE 'DrugID' AND object_ID IN (SELECT object_id FROM sys.tables WHERE Name = 'PatientDrugMapping'))
-BEGIN 
-	ALTER TABLE PatientDrugMapping ADD DrugID INT 
-END
-ELSE
-	PRINT 'Already DrugID Column exists in PatientDrugMapping table'
